f7831bc778139f59336b9256215573b1fd5a55a1
Adam Urbina <adamurbina@gmail.com>
2018-05-31 19:36:51 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_find(collection)
+    index = 0
+    results = []
 
+    while index < collection.size
+        if yield(collection[index])
+            results << collection[index]
+        end
+        index += 1
+    end
+    results
 end
